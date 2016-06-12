77740c64f661e908ae96e523a5d4baff7e91e2bb
soumyashree1990 <soumyashree1990@gmail.com>
2016-06-12 20:23:31 +0530

    my first commit - Added myNewMethod()

--- HelloWorld/src/main/java/com/sonymobile/integration/App.java
+++ HelloWorld/src/main/java/com/sonymobile/integration/App.java
@@ -9,5 +9,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        new App().myNewMethod("test");
+    }
+
+    public void myNewMethod(String input)
+    {
+
+        System.out.println("Inside Mynewmethod");
     }
 }
+
+
+
